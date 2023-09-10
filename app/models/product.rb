f92e2b01c92db6d8dee3aa12b0c723b02be7ef1f
hardow2011 <hardow2011@outlook.com>
2023-09-10 11:50:33 -0400

    feat: allow nested attributes for product_options to create them with the product

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,12 +13,12 @@
 #  index_products_on_permalink  (permalink) UNIQUE
 #
 class Product < ApplicationRecord
-  after_create :create_options
   before_save :generate_permalink
 
   attr_accessor :quantity, :price, :description, :hidden
 
   has_many :product_options, dependent: :delete_all
+  accepts_nested_attributes_for :product_options
 
   def as_json(options = nil)
     super(except: [:id, :created_at, :updated_at],
@@ -28,16 +28,6 @@ class Product < ApplicationRecord
   end
 
   private
-
-  def create_options
-    product_option = ProductOption.create(name: 'Standard', hidden: self.hidden, description: self.description, price: self.price, quantity: self.quantity)
-    product_option.product = self
-
-    unless product_option.save
-      errors.add(:base, 'There was an error creating the option')
-      ActiveRecord::Rollback
-    end
-  end
 
   def generate_permalink
     self.permalink = self.name.parameterize
