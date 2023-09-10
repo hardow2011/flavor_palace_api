4618b4396139fb90721ac45e8f872bc19c985e0f
hardow2011 <hardow2011@outlook.com>
2023-09-09 22:57:11 -0400

    feat: added accessors to allow params in controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,17 @@
 #  updated_at :datetime         not null
 #
 class Product < ApplicationRecord
+  after_create :create_options
+
+  attr_accessor :quantity, :price, :description, :hidden
+
+  def create_options
+    product_option = ProductOption.create(name: 'Standard', hidden: self.hidden, description: self.description, price: self.price, quantity: self.quantity)
+    product_option.product = self
+
+    unless product_option.save
+      errors.add(:base, 'There was an error creating the option')
+      ActiveRecord::Rollback
+    end
+  end
 end
