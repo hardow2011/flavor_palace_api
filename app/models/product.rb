aa08b86e60fe1dd6308c35cc1d13e759b6698005
hardow2011 <hardow2011@outlook.com>
2023-09-09 23:11:52 -0400

    feat: generate permalink for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,13 +4,19 @@
 #
 #  id         :bigint           not null, primary key
 #  name       :string
+#  permalink  :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class Product < ApplicationRecord
   after_create :create_options
+  before_save :generate_permalink
 
   attr_accessor :quantity, :price, :description, :hidden
+
+  has_many :product_options, dependent: :delete_all
+
+  private
 
   def create_options
     product_option = ProductOption.create(name: 'Standard', hidden: self.hidden, description: self.description, price: self.price, quantity: self.quantity)
@@ -20,5 +26,9 @@ class Product < ApplicationRecord
       errors.add(:base, 'There was an error creating the option')
       ActiveRecord::Rollback
     end
+  end
+
+  def generate_permalink
+    self.permalink = self.name.parameterize
   end
 end
