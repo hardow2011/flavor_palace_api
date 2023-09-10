2363c710cada5c1d47439ee3be57225e31801ab2
hardow2011 <hardow2011@outlook.com>
2023-09-10 12:14:17 -0400

    feat: changed product_options in the json response for product_options_attributes

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,18 +16,23 @@ class Product < ApplicationRecord
   before_save :generate_permalink
 
   attr_accessor :quantity, :price, :description, :hidden
+  attr_readonly :product_options_attributes
 
   has_many :product_options, dependent: :delete_all
   accepts_nested_attributes_for :product_options
 
   def as_json(options = nil)
     super(except: [:id, :created_at, :updated_at],
-          include: { product_options:
+          include: { product_options_attributes:
                        { except:
                            [:id, :created_at, :updated_at] } })
   end
 
   private
+
+  def product_options_attributes
+    self.product_options
+  end
 
   def generate_permalink
     self.permalink = self.name.parameterize
