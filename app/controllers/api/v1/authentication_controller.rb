580619958976f81901a02f4088c3b9b61c374ccd
hardow2011 <hardow2011@outlook.com>
2023-09-16 11:39:21 -0400

    fix: skip authentication for user login

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::AuthenticationController < ApplicationController
+  skip_before_action :authenticate_request!
   def create
     user = User.find_or_create_by!(email: params[:user][:email])
     user.send_magic_link
